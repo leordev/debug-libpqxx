fa165b5ee97c13fbfbd9b932edcf1468787fcba9
Leo Ribeiro <leordev@gmail.com>
2020-01-29 10:26:39 -0500

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,5 @@
-#include <division.h>
+#include <iostream>
 #include <pqxx/pqxx>
-
-static const char *const HEADER = "\nDivider © 2018 Monkey Claps Inc.\n\n";
-static const char *const USAGE = "Usage:\n\tdivider <numerator> <denominator>\n\nDescription:\n\tComputes the result of a fractional division,\n\tand reports both the result and the remainder.\n";
 
 int main(int argc, const char *argv[]) {
   std::cout << "connect to postgresql... ";
@@ -21,11 +18,14 @@ int main(int argc, const char *argv[]) {
   std::cout << "\n    " << add_123;
   pipeline.insert(add_123);
 
-  // INVALID STATEMENT >> SHOULD THROW!
-  // std::string add_999 = "INSERT aaaa"; // throws: libc++abi.dylib: terminating with uncaught exception of type pqxx::usage_error: Attempt to commit previously aborted transaction<READ COMMITTED>
+  // WRONG, IT'S COMMITTING SUCCESSFULLY, EVEN THOUGH WE HAVE A SQL ERROR: column "dummy" of relation "my_test" does not exist
   std::string add_999 = "INSERT INTO my_test (my_num, dummy) VALUES (999, 'dummy str')"; // DOES NOT THROW ANYTHING!
   std::cout << "\n    " << add_999;
   pipeline.insert(add_999);
+
+  // PS: if I do something like below, it throws CORRECTLY
+  // libc++abi.dylib: terminating with uncaught exception of type pqxx::usage_error: Attempt to commit previously aborted transaction<READ COMMITTED>
+  // std::string add_999 = "INSERT aaaa";
 
   pipeline.complete();
   trx.commit();
@@ -33,29 +33,3 @@ int main(int argc, const char *argv[]) {
 
   return 0;
 }
-
-// int main(int argc, const char *argv[]) {
-//   Fraction f;
-
-//   std::cout << HEADER;
-
-//   // ensure the correct number of parameters are used.
-//   if (argc < 3) {
-//     std::cout << USAGE;
-//     return 1;
-//   }
-
-//   f.numerator = atoll(argv[1]);
-//   f.denominator = atoll(argv[2]);
-
-//   Division d = Division(f);
-//   try {
-//     DivisionResult r = d.divide();
-
-//     std::cout << "Division : " << f.numerator << " / " << f.denominator << " = " << r.division << "\n";
-//     std::cout << "Remainder: " << f.numerator << " % " << f.denominator << " = " << r.remainder << "\n";
-//   } catch (DivisionByZero) {
-//     std::cout << "Can not divide by zero, Homer. Sober up!\n";
-//   }
-//   return 0;
-// }
